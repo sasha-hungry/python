855618d6b4ea17930376b006f507861d1288faf7
sasha-hungry <argus2005@inbox.ru>
2021-08-28 04:20:55 -0500

    выполнено урок 1 задание 6

---
+++ home-work-1/task-6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+a = float(input('введите результат первого дня (a): '))
+b = float(input('введите целевой результат (b): ')) 
+count = 1
+
+while a < b :
+   a = a + a * 0.1
+   count = count + 1
+   if a >= b :
+       break
+
+print(f'На день {count} спортсмен пробежал не менеем {b} километров')
