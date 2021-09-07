d238e1be4a8d6ed771acd2edd1a22b9fddf8b436
sasha-hungry <argus2005@inbox.ru>
2021-09-07 16:08:04 +0300

    fixed bag in task 4-5

--- home-work-4/task4-5.py
+++ home-work-4/task4-5.py
@@ -13,7 +13,7 @@ from functools import reduce
 def multiplication(arg1, arg2):
     return(arg1 * arg2)
 
-input_list = [ element for element in range(100, 1001)]
+input_list = [ element for element in range(100, 1001) if element % 2 == 0]
 
 # вариант со своей функцией
 result = reduce(multiplication, input_list)
