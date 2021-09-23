ff4f675e5fc7586ce404e7a3427e02e721003ab3
sasha-hungry <argus2005@inbox.ru>
2021-09-23 19:50:30 +0300

    done  tree type of class, check input and some type of storage lesson 8 tusk 4-5-6

--- home-work-8/task-4-5-6.py
+++ home-work-8/task-4-5-6.py
@@ -15,8 +15,17 @@
 
 class Warehouse:
     
+    storage = dict()
+    
     def __init__(self):
-        self.storage = dic()
+        pass
+        
+    def income(self, position, items):
+        self.position = position
+        self.items = items
+        self.storage[self.position] = items
+        return self.storage
+        
 
 
 class OfficeEq:
@@ -29,19 +38,44 @@ class OfficeEq:
 
 class Printer(OfficeEq):
     
-    def __init__(self, name, price, color, type_priter):
+    def __init__(self, name, price, color, type_printer):
         self.name = name
         self.price = price
         self.color = color
-        self.type_priter = type_priter
+        self.type_printer = type_printer
+    
+    def validate(self, name, price, color, type_printer):
+        if type(self.price) == int:
+            if type(self.color) == bool:
+                items = { 'name':self.name, 'price':self.price, 'color':self.color, 'type_printer': type_printer }
+                return items
+            else:
+                print('color должен принимать значения True или False')
+                return None
+        else:
+            print('цена должна быть цифрой')
+            return None
+           
 
 class Scan(OfficeEq):
     
-    def __init__(self, name, price, color, size):
+    def __init__(self, name, price, color):
         self.name = name
         self.price = price
         self.color = color
-        self.size = size
+        
+    def validate(self, name, price, color):
+        if type(self.price) == int:
+            if type(self.color) == bool:
+                items = { 'name':self.name, 'price':self.price, 'color':self.color }
+                return items
+            else:
+                print('color должен принимать значения True или False')
+                return None
+        else:
+            print('цена должна быть цифрой')
+            return None
+
 
 
 class Copier(OfficeEq):
@@ -50,9 +84,25 @@ class Copier(OfficeEq):
         self.name = name
         self.price = price
         self.size = size
+        
+    def validate(self, name, price, size):
+        if type(self.price) == int:
+            items = { 'name':self.name, 'price':self.price, 'size':self.size }
+            return items
+        else:
+            print('цена должна быть цифрой')
+            return None
 
 
 my_printer = Printer('HP', 1000, True, 'laser')
-print(my_printer.__dict__)
-     
-     
+my_scaner = Scan('HP',500, True)
+my_copier = Copier('Xerox', 2000, 'A3')
+
+# print(type(my_printer), my_printer.__dict__)
+# print(my_printer.validate('HP', 1000, True, 'laser'))
+
+my_warehouse = Warehouse()
+
+my_warehouse.income('printers', my_printer.validate('HP', 1000, True, 'laser'))
+my_warehouse.income('scaners', my_scaner.validate('HP', 500, True))
+my_warehouse.income('copier', my_copier.validate('Xerox', 2000, 'A3'))
